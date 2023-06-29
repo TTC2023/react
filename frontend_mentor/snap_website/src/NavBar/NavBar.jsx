41fe2d9a69be2ee6138e0017b17da45629c7a900
Tyler Correll <corrett10@gmail.com>
2023-06-28 18:45:52 -0600

    new website

--- frontend_mentor/snap_website/src/NavBar/NavBar.jsx
+++ frontend_mentor/snap_website/src/NavBar/NavBar.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import styles from './NavBar.module.css'
+import logo from '../assets/logo.svg'
+import { useState } from 'react'
+import arrowUp from '../assets/icon-arrow-up.svg'
+import arrowDown from '../assets/icon-arrow-down.svg'
 
 const NavBar = () => {
+
+  const [features, setfeatures] = useState(false)
+  const [companies, setCompanies] = useState(false)
+
+  const toggleArrow = (num) => {
+    if(num===1){
+      setfeatures(!features)
+    }
+    if(num===2){
+      setCompanies(!companies)
+    }
+  }
+
   return (
     <div className={styles.navBar}>
-        <div className={styles.links}>NavBar</div>
+      <div className={styles.links}>
+        <img className={styles.logo} src={logo} alt="logo" />
+        <div>
+          <a href="/">Features</a>
+          <img onClick={(e)=>toggleArrow(1)} src={features ? arrowUp : arrowDown} alt="features" />
+        </div>
+        <div>
+          <a href="/">Company</a>
+          <img onClick={(e)=>toggleArrow(2)} src={companies ? arrowUp : arrowDown} alt="companies" />
+        </div>
+        <a href="/">Careers</a>
+        <a href="/">About</a>
+      </div>
+      <div className={styles.signIn}>
+        <button className={styles.login}>Login</button>
+        <button className={styles.register}>Register</button>
+      </div>
     </div>
   )
 }
