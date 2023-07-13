1bb5618b85e68fea9eed1d5ff1a2ca90d763c472
Tyler Correll <corrett10@gmail.com>
2023-07-13 15:52:41 -0500

    needs more work

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -1,106 +1,59 @@
 import React, { useState } from 'react'
 import styles from './Projects.module.css'
-import { GoLinkExternal } from "react-icons/go"
-import { AiFillGithub } from "react-icons/ai";
-import car from '../../assets/car_company.png'
+import e_commerce from '../../assets/e-commerce.png'
 import snap from '../../assets/snap.png'
 import credit_card from '../../assets/credit_card.png'
 import newsletter from '../../assets/newsletter.png'
 import quotes from '../../assets/quotes.png'
 import calculator from '../../assets/calculator.png'
+import next from '../../assets/icon-next.svg'
+import previous from '../../assets/icon-previous.svg'
 
 const Experience = () => {
-  // Initialize with an array for each card's background state
-  const [background, setBackground] = useState([true, true, true, true, true, true])
 
-  const backgroundToggle = (number) => {
-    setBackground((prevBackground) => {
-      const newState = [...prevBackground];
-      newState[number] = !newState[number];
-      return newState;
-    });
-    console.log(background);
+  const [selected, setSelected] = useState(0)
+  const image = [e_commerce, calculator, snap, credit_card, newsletter, quotes]
+  const header = ["E-Commerce Website", "Functioning Calculator", "Landing Webpage", "Credit Card Form", "NewsLetter", "Quotes API"]
+  const paragraph = [
+    "I developed a full-functioning product page for an e-commerce website.",
+    "I developed a fully operational calculator.",
+    "I developed a well-designed SEO-friendly landing page",
+    "I developed a verification form for credit card information.",
+    "I developed a newsletter from a pre-made wireframe.",
+    "I developed a random inspirational quotes page using a third-party API"
+  ]
+  const link = ['https://frontend-mentor-1-six.vercel.app/','','https://react-6qip.vercel.app/', 'https://react-qobb.vercel.app/', '','']
+
+  const handleNext = () => {
+    if(selected < 5){
+      let newSelected = selected + 1
+      setSelected(newSelected)
+    }
+  }
+
+  const handlePrevious = () => {
+    if(selected > 0){
+      let newSelected = selected - 1
+      setSelected(newSelected)
+    }
   }
 
   return (
     <div className={styles.experiencePage}>
       <div className={styles.container}>
-        <div className={styles.card}>
-          {background[0] ? 
-            <img onMouseEnter={() => backgroundToggle(0)} className={styles.cardImg} src={car} alt='car'></img> :
-            <div onMouseLeave={() => backgroundToggle(0)}>
-              <h2 className={styles.content}>Car Company</h2>
-              <p>I designed and implemented an interactive car sales website.</p>
-              <div className={styles.icons}>
-                <a href="https://react-edo6bzxoh-ttc2023.vercel.app/" target="_blank" rel="noopener noreferrer" className={styles.link}><GoLinkExternal size={20} /></a>
-              </div>
+        <div className={styles.project}>
+          <img className={styles.mainImg} src={image[selected]} alt='mainImg'></img>
+          <div className={styles.description}>
+            <h2>{header[selected]}</h2>
+            <p>{paragraph[selected]}</p>
+            <a href={link[selected]} target="_blank" rel="noopener noreferrer">See live website</a>
+            <div className={styles.arrows}>
+              <img onClick={()=>handlePrevious()} src={previous} alt="previous" />
+              <img onClick={()=>handleNext()} src={next} alt="next" />
             </div>
-          }
-        </div>
-        <div className={styles.card}>
-          {background[1] ? 
-            <img onMouseEnter={() => backgroundToggle(1)} className={styles.cardImg} src={calculator} alt='calculator'></img> :
-            <div onMouseLeave={() => backgroundToggle(1)}>
-              <h2 className={styles.content}>Calculator</h2>
-              <p>I crafted a fully functioning calculator with an intuitive user interface.</p>
-              <div className={styles.icons}>
-                <div><AiFillGithub size={20} /></div>
-                <a href="https://github.com/TTC2023/react/tree/master/calculator" target="_blank" rel="noopener noreferrer" className={styles.link}><GoLinkExternal size={20} /></a>
-              </div>
-            </div>
-          }
-        </div>
-        <div className={styles.card}>
-          {background[2] ? 
-            <img onMouseEnter={() => backgroundToggle(2)} className={styles.cardImg} src={newsletter} alt='news'></img> :
-            <div onMouseLeave={() => backgroundToggle(2)}>
-              <h2 className={styles.content}>NewsLetter</h2>
-              <p>I developed a mock newsletter subscription service.</p>
-              <div className={styles.icons}>
-                <div><AiFillGithub size={20} /></div>
-                <a href="https://github.com/TTC2023/react/tree/master/music_player" target="_blank" rel="noopener noreferrer" className={styles.link}><GoLinkExternal size={20} /></a>
-              </div>
-            </div>
-          }
-        </div>
-        <div className={styles.card}>
-          {background[3] ? 
-            <img onMouseEnter={() => backgroundToggle(3)} className={styles.cardImg} src={snap} alt='age'></img> :
-            <div onMouseLeave={() => backgroundToggle(3)}>
-              <h2 className={styles.content}>Snap Website</h2>
-              <p>I developed a frontend website from a wireframe.</p>
-              <div className={styles.icons}>
-                <a href="https://react-bdba.vercel.app/" target="_blank" rel="noopener noreferrer" className={styles.link}><GoLinkExternal size={20} /></a>
-              </div>
-            </div>
-          }
-        </div>
-        <div className={styles.card}>
-          {background[4] ? 
-            <img onMouseEnter={() => backgroundToggle(4)} className={styles.cardImg} src={credit_card} alt='cc'></img> :
-            <div >
-              <h2 className={styles.content}>Credit Card Website</h2>
-              <p onMouseLeave={() => backgroundToggle(4)}>I developed a credit card form that validates the inputs.</p>
-              <div className={styles.icons}>
-                <a href="https://react-qobb.vercel.app/" target="_blank" rel="noopener noreferrer" className={styles.link}><GoLinkExternal size={20} /></a>
-              </div>
-            </div>
-          }
-        </div>
-        <div className={styles.card}>
-          {background[5] ? 
-            <img onMouseEnter={() => backgroundToggle(5)} className={styles.cardImg} src={quotes} alt='quotes'></img> :
-            <div onMouseLeave={() => backgroundToggle(5)}>
-              <h2 className={styles.content}>Quotes API</h2>
-              <p>I developed a quotes generator using a third party API.</p>
-              <div className={styles.icons}>
-                <div><AiFillGithub size={20} /></div>
-                <a href="https://github.com/TTC2023/react/tree/master/quotes_api" target="_blank" rel="noopener noreferrer" className={styles.link}><GoLinkExternal size={20} /></a>
-              </div>
-            </div>
-          }
           </div>
         </div>
+      </div>
     </div>
   )
 }
